e9c6359d24ef74bbb8c6bea4a2f087588cd6498a
Diana-Cano <dianacanoo@icloud.com>
2022-12-19 23:43:19 -0800

    completed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
+function App(){
+    let [colors, setColors] = useState([
+      'violet', 'blue', 
+      'lightblue', 'green',
+      'greenyellow', 'yellow', 
+      'orange', 'red'])
+    const addColor = (newColor)=>{
+      setColors([...colors,newColor])
+    }
+    let colorMap = colors.map ((color, i ) => {
+      return (
+        <ColorBlock color= {color} key = {i}/>
+
+      )
+    })
+    return (
+        <div className="App">
+          {colorMap}
+          <ColorForm addColor = {addColor}/>
+        </div>
+    )
+}
+
+export default App;
